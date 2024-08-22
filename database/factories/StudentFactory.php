ca4a8ed619f2c591c8b85d4f242c73af0753fcdc
Jhziel <barnedodonjaziel57@gmail.com>
2024-08-22 14:16:51 +0800

    Factory-Seeder

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'last_name' => fake()->lastName(),
+            'first_name' => fake()->firstName(),
+            'age' => fake()->numberBetween(18, 30),
+            'stud_id' => fake()->unique()->numberBetween(100000, 999999),
+        ];
+    }
+}
