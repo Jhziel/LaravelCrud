94928d90a6d92bc754cf383b87a3bff555504152
Jhziel <barnedodonjaziel57@gmail.com>
2024-08-23 17:05:36 +0800

    Create View

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>LaravelCrud</title>
-     @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/css/inputField.css'])
 </head>
 
 <body class="bg-slate-900 px-28">
